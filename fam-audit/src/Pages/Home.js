dfb09d731cc0604e1eb45cfe7bbddebe35a28903
zaurkhalilli <ggencetel274@gmail.com>
2025-05-12 01:24:19 -0700

    HomeMap elave olundu

--- fam-audit/src/Pages/Home.js
+++ fam-audit/src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeVideo from '../Components/HomeVideo/HomeVideo'
-import Hometeqdim from '../Components/HomeTeqdim/Hometeqdim'
+import Hometeqdim from '../Components/HomeTeqdimat/Hometeqdim'
+import Homemap from '../Components/HomeMap/Homemap'
 
 const Home = () => {
   return (
     <div>
       <HomeVideo/>
       <Hometeqdim/>
+      <Homemap/>
     </div>
   )
 }
