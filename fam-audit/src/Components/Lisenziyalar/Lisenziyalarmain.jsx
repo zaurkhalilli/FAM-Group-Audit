213220c77b5a9fce84bf9d84b1aa6d0581a38ea2
zaurkhalilli <ggencetel274@gmail.com>
2025-05-15 02:50:21 -0700

    audit lisenziyalari elave olundu

--- fam-audit/src/Components/Lisenziyalar/Lisenziyalarmain.jsx
+++ fam-audit/src/Components/Lisenziyalar/Lisenziyalarmain.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import '../Lisenziyalar/Lisenziyalarmain.scss'
+import Photoaudit1 from '../Lisenziyalar/audit1.jpeg' 
+import Photoaudit2 from '../Lisenziyalar/audit2.jpeg' 
+import Photoaudit3 from '../Lisenziyalar/audit3.jpg' 
 const Lisenziyalarmain = () => {
   return (
     <div id='lisenziyalar'>
-      adsasdasdad
+      <h2>FAM Group Audit MMC-nin lisenziyaları : </h2>
+      <div className="container">
+        <div className="row g-6">
+          <div className="col-xl-4">
+            <p>1. 10 Oktyabr 2024-cü ildə Azərbaycan Respublikası Auditorlar Platası tərəfindən Auditor fəaliyyətinin icrası ilə bağlı lisenziya.</p>
+            <br />
+          <img src={Photoaudit1} alt="" />
+          </div>
+          <div className="col-xl-4">
+            <p>2. 11 Yanvar 2023-cü ildə Azərbaycan Respublikası Auditorlar Palatası tərəfindən Auditor fəaliyyətinin icrası ilə bağlı icazə lisenziya. </p>
+          <img src={Photoaudit2} alt="" />
+          </div>
+          <div className="col-xl-4">
+            <p>3. Audit üzrə baş mütəxəssisimiz Zaur Salahovun ACCA Kvalifikasiya sənədi. </p>
+            <br />
+            <br />
+          <img src={Photoaudit3} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
